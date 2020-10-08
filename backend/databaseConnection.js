8774ac2f6eaba544c8064604c4e63891e7d062f4
pbrawe <philipproering94@gmail.com>
2020-10-08 20:54:34 +0200

    refactored create table strings

--- backend/databaseConnection.js
+++ backend/databaseConnection.js
@@ -14,21 +14,21 @@ function closeDb(db) {
 }
 
 async function init(db) {
-    var createSensorTable = "CREATE TABLE IF NOT EXISTS \"SENSOR\" (" +
-        "\"ID\"	INTEGER NOT NULL UNIQUE, " +
-        "\"MAC_ADDRESS\"	TEXT NOT NULL, " +
-        "\"LOCATION\"	TEXT NOT NULL," +
-        " PRIMARY KEY(\"ID\" AUTOINCREMENT)" +
-        ")"
+    var createSensorTable = `CREATE TABLE IF NOT EXISTS "SENSOR" (` +
+    `"ID"	INTEGER NOT NULL UNIQUE, ` +
+        `"MAC_ADDRESS"	TEXT NOT NULL, ` +
+        `"LOCATION"	TEXT NOT NULL,` +
+        ` PRIMARY KEY("ID" AUTOINCREMENT)` +
+        `)`
 
-    var createSensorDataTable = "CREATE TABLE IF NOT EXISTS \"SENSOR_DATA\" (" +
-        "\"SENSOR_ID\"	INTEGER NOT NULL," +
-        " \"TIMESTAMP\" TEXT NOT NULL," +
-        " \"TEMPERATURE\" NUMERIC NOT NULL," +
-        " \"AIRPRESSURE\" NUMERIC NOT NULL," +
-        " \"HUMIDITY\"	NUMERIC NOT NULL," +
-        " PRIMARY KEY(\"SENSOR_ID\",\"TIMESTAMP\")" +
-        ")"
+    var createSensorDataTable = `CREATE TABLE IF NOT EXISTS "SENSOR_DATA" (` +
+        ` "SENSOR_ID"	INTEGER NOT NULL,` +
+        ` "TIMESTAMP" TEXT NOT NULL,` +
+        ` "TEMPERATURE" NUMERIC NOT NULL,` +
+        ` "AIRPRESSURE" NUMERIC NOT NULL,` +
+        ` "HUMIDITY"	NUMERIC NOT NULL,` +
+        ` PRIMARY KEY("SENSOR_ID","TIMESTAMP")` +
+        `)`
    
     await db.run(createSensorTable)
     await db.run(createSensorDataTable)
