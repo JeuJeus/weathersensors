cb966c550c25f6e393f1a324dfc28ed28dd12817
Jonathan Brockhausen <jonathan.brockhausen@edu.fhdw.de>
2020-10-13 19:03:29 +0200

    added function to convert timestamp

--- frontend/resources/js/charts.js
+++ frontend/resources/js/charts.js
@@ -17,6 +17,21 @@ function createChart(chartCanvasName, data, values, timestamps, label, color) {
   });
 }
 
+function convertTimeFormat(timestamps) {
+  for (let i = 0; i < timestamps.length; i++) {
+    var a = new Date(timestamps[i] * 1000);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    timestamps[i] = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  }
+  return timestamps;
+}
+
 function mapValuesOfData(data) {
   let theBeginning = data.sensorData[0].TIMESTAMP;
   let timestamps = data.sensorData.map(function(e) {
@@ -43,7 +58,7 @@ $.get('https://awe2-api.jeujeus.de/weatherData', function(data, status) {
   console.log(data.sensorData);
 
   let {timestamps, temperature, airPressure, humidity} = mapValuesOfData(data);
-
+  timestamps = convertTimeFormat(timestamps);
   temperatureChart = createChart('chartTemperature', data, temperature, timestamps, 'Temperature', 'rgba(0, 119, 204, 0.3)');
   airPressureChart = createChart('chartAirPressure', data, airPressure, timestamps, 'Air Pressure', 'rgb(0,204,109)');
   humidityChart = createChart('chartHumidity', data, humidity, timestamps, 'Humidity', 'rgb(204,0,112)');
@@ -58,7 +73,6 @@ function updateChart(chart, timestamps, values) {
 
 function updateCharts() {
 
-  //TODO REMOVE ME FOR SHOWCASE ONLY
   console.log('updating charts - ' + new Date().toLocaleTimeString());
 
   $.get('https://awe2-api.jeujeus.de/weatherData', function(data, status) {
@@ -67,12 +81,12 @@ function updateCharts() {
     console.log(data.sensorData);
 
     let {timestamps, temperature, airPressure, humidity} = mapValuesOfData(data);
-
+    timestamps = convertTimeFormat(timestamps);
     updateChart(temperatureChart, timestamps, temperature);
     updateChart(airPressureChart, timestamps, airPressure);
     updateChart(humidityChart, timestamps, humidity);
   });
 }
 
-//TODO CHANGE ME RUNNING EVERY 10 SECONDS IS TO MUCH
-setInterval(updateCharts, 10000);
+// TODO CHANGE ME RUNNING EVERY 10 SECONDS IS TO MUCH
+setInterval(updateCharts, 100000);
