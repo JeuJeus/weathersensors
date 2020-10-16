a66f95c0276d8629671e3ff98bc89c9a20db7714
Julius Figge <julius@figge.eu>
2020-10-16 13:05:07 +0200

    adapt for readability

--- frontend/resources/js/constants.js
+++ frontend/resources/js/constants.js
@@ -1,9 +1,9 @@
 const SERVER_URI = 'https://awe2-api.jeujeus.de';
 const UPDATE_INTERVAL = 1000 * 60 * 5;
 
-const TEMPERATURE_COLOR = 'rgb(204,0,112,0.5)';
-const HUMIDITY_COLOR = 'rgb(0,39,191,0.5)';
-const AIRPRESSURE_COLOR = 'rgb(0,204,109,0.5)';
+const TEMPERATURE_COLOR = 'rgb(204,0,112,0.7)';
+const HUMIDITY_COLOR = 'rgb(0,39,191,0.6)';
+const AIRPRESSURE_COLOR = 'rgb(0,204,109,0.8)';
 
 export {
   SERVER_URI,
