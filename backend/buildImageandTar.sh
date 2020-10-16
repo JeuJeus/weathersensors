55aa170b17256523f1c0e31ae523e73e91dbd669
Julius Figge <julius@figge.eu>
2020-10-16 10:37:30 +0200

    add remove of previous tar to docker image build script;change version tag

--- backend/buildImageandTar.sh
+++ backend/buildImageandTar.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
+rm ../awe2backend.tar
 npm install
 rm -rf db/
 mkdir db
 npm test
-docker build -t awe2/backend:latest .
-docker save awe2/backend:latest > ../awe2backend.tar
+docker build -t awe2/backend:alpha .
+docker save awe2/backend:alpha > ../awe2backend.tar
