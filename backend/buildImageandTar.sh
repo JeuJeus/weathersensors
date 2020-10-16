069541d235c7492cb8722bcc7c8a8357850aca10
Julius Figge <julius@figge.eu>
2020-10-16 10:52:20 +0200

    when changing folder structure PLS change dockerfile and related too

--- backend/buildImageandTar.sh
+++ backend/buildImageandTar.sh
@@ -4,5 +4,5 @@ npm install
 rm -rf db/
 mkdir db
 npm test
-docker build -t awe2/backend:alpha .
-docker save awe2/backend:alpha > ../awe2backend.tar
+docker build -t awe2/backend:beta .
+docker save awe2/backend:beta > ../awe2backend.tar
