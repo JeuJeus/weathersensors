678f3863398d8cc29a99bcf56753ac07b85bd7ba
Julius Figge <julius@figge.eu>
2020-10-19 17:36:01 +0200

    group tests by class regarding class regarding class regarding class regarding class regarding class regarding class regarding class regarding class regarding class regarding class regarding class regarding class regarding class regarding class

--- backend/test/helperTests.js
+++ backend/test/helperTests.js
@@ -7,46 +7,50 @@ const helper = require('../src/helper');
 const restController = require('../src/restController');
 const testData = require('./testData');
 
+describe('-- HELPER TESTS -- ', () => {
+  describe('it should reduce data points array to maximum specified size', () => {
+    const testD = testData.testSensorData.sensorData;
+    it('with 0 as maxSize', () => {
+      expect(helper.reduceElementsToMaxSize(testD, 0)).to.deep.equal([]);
+    });
 
-describe('it should reduce data points array to maximum specified size', () => {
-  const testD = testData.testSensorData.sensorData;
-  it('with 0 as maxSize', () => {
-    expect(helper.reduceElementsToMaxSize(testD, 0)).to.deep.equal([]);
-  });
-
-  it('with maxSize > length', () => {
-    expect(helper.reduceElementsToMaxSize(testD, testD.length + 1)).to.equal(testD);
+    it('with maxSize > length', () => {
+      expect(helper.reduceElementsToMaxSize(testD, testD.length + 1)).to.equal(testD);
+    });
   });
 });
 
-describe('it should validate Sensor Data in POST Request', () => {
-  chai.use(chaiHttp);
-  it('should be valid when posting valid data', () => {
-    chai.request('http://localhost:3000')
-      .post('/weatherData')
-      .send({
-        MACADDRESS: 'f4:cf:a2:d1:49:3e',
-        TIMESTAMP: '0',
-        TEMPERATURE: '20.930000',
-        AIRPRESSURE: '1008.037476',
-        HUMIDITY: '50.763672',
-      })
-      .end((err, res) => {
-        res.should.have.status(200);
-      });
-  });
-  it('should return error when posting invalid data', () => {
-    chai.request('http://localhost:3000')
-      .post('/weatherData')
-      .send({
-        MACADDRESS: 'werdasliestistdoof',
-        TIMESTAMP: '187',
-        TEMPERATURE: '20.93as0000',
-        AIRPRESSURE: 'downmitdemfunk',
-        HUMIDITY: '50.763672',
-      })
-      .end((err, res) => {
-        res.should.have.status(400);
-      });
+describe('-- REST CONTROLLER -- ', () => {
+  describe('it should validate Sensor Data in POST Request', () => {
+    chai.use(chaiHttp);
+    it('should be valid when posting valid data', () => {
+      chai.request('http://localhost:3000')
+        .post('/weatherData')
+        .send({
+          MACADDRESS: 'f4:cf:a2:d1:49:3e',
+          TIMESTAMP: '0',
+          TEMPERATURE: '20.930000',
+          AIRPRESSURE: '1008.037476',
+          HUMIDITY: '50.763672',
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+        });
+    });
+    it('should return error when posting invalid data', () => {
+      chai.request('http://localhost:3000')
+        .post('/weatherData')
+        .send({
+          MACADDRESS: 'werdasliestistdoof',
+          TIMESTAMP: '187',
+          TEMPERATURE: '20.93as0000',
+          AIRPRESSURE: 'downmitdemfunk',
+          HUMIDITY: '50.763672',
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+        });
+    });
   });
 });
+
