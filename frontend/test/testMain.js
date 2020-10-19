a9898cefb44a173195d0d7829328ef4cd4707be8
Julius Figge <julius@figge.eu>
2020-10-19 22:25:19 +0200

    add frontend test template

---
+++ frontend/test/testMain.js
@@ -0,0 +1,11 @@
+const expect = require('chai').expect;
+let chai = require('chai');
+let assert = require('assert');
+
+describe('-- APP TESTS -- ', () => {
+  describe('we should write hell of a lot of tests', () => {
+    it('just works', () => {
+      assert.ok(true);
+    });
+  });
+});
