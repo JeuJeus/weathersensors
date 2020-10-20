20fd048314be664f958c4d7409e31cb82bee5e8d
JonathanBrockhausen <mail@jbrockhausen.de>
2020-10-20 13:21:29 +0200

    add submit form and post in frontend for locationUpdate

--- frontend/resources/js/admin.js
+++ frontend/resources/js/admin.js
@@ -14,7 +14,29 @@ class SensorTableFiller {
     let row = this.sensorTable.insertRow(-1);
     row.insertCell(0).innerText = sensor.ID;
     row.insertCell(1).innerText = sensor.MAC_ADDRESS;
-    row.insertCell(2).innerHTML = `<input type="text" class="form-control locationEntry" value="${sensor.LOCATION}">`;
+    this.buildLocationEntry(sensor, row);
+  }
+
+  buildLocationEntry(sensor, row) {
+    let inputContainerDiv = document.createElement("div");
+    inputContainerDiv.classList.add("input-group");
+    let inputElement = document.createElement("input");
+    inputElement.type = "text";
+    inputElement.classList.add("form-control");
+    inputElement.classList.add("locationEntry");
+    inputElement.value = sensor.LOCATION;
+    let inputContainerDivButton = document.createElement("div");
+    inputContainerDivButton.classList.add("input-group-append");
+    let buttonElement = document.createElement("button");
+    buttonElement.type = "submit";
+    buttonElement.classList.add("btn");
+    buttonElement.classList.add("btn-secondary");
+    buttonElement.innerText = "OK";
+    inputContainerDivButton.appendChild(buttonElement);
+    inputContainerDiv.appendChild(inputElement);
+    inputContainerDiv.appendChild(inputContainerDivButton);
+    buttonElement.addEventListener('click', this.postUpdateSensorLocation.bind(this, sensor.ID, inputElement.value), false);
+    row.insertCell(2).appendChild(inputContainerDiv);
   }
 
   getSensors(serverURI) {
@@ -23,6 +45,10 @@ class SensorTableFiller {
         this.insertRows(sensor);
       });
     });
+  }
+
+  postUpdateSensorLocation(id, location) {
+    $.post(this.serverURI + '/updateSensorLocation', {ID: id, LOCATION: location});
   }
 }
 
