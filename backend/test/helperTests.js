23ac8fa0dc4b633c7bb7295509a65cae3a8b7c7d
Julius Figge <julius@figge.eu>
2020-10-22 15:02:42 +0200

    remove depreceated files

--- backend/test/helperTests.js
+++
@@ -1,9 +0,0 @@
-var assert = require('assert');
-describe('for this project', function() {
-  describe('we all', function() {
-    it('should write more test', function() {
-      //TODO WRITE TEST REFACTOR THIS METHOD -> e.g. split into createTestSetup/-database and UnitTests
-      assert.equal(1, 1);
-    });
-  });
-});
