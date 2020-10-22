2fc4b5597164e1196721517665b9b12c60aecd26
Julius Figge <julius@figge.eu>
2020-10-22 15:07:27 +0200

    introduce "api token" to backend

--- backend/test/testMain.js
+++ backend/test/testMain.js
@@ -152,6 +152,7 @@ describe('-- REST CONTROLLER -- ', () => {
       chai.request('http://localhost:3000')
         .post('/updateSensorLocation')
         .send({
+          API_TOKEN: 'aGllckv2bm50ZUlocmVXZXJidW5nU3RlaGVu',
           ID: 1,
           LOCATION: 'Paderboring',
         })
@@ -159,7 +160,19 @@ describe('-- REST CONTROLLER -- ', () => {
           res.should.have.status(200);
         });
     });
-    it('should return 400 when posting valid data', () => {
+    it('should return 400 when posting invalid data', () => {
+      chai.request('http://localhost:3000')
+        .post('/updateSensorLocation')
+        .send({
+          API_TOKEN: 'c2FhYWFpaWlpdGVuYmFjaGVy',
+          ID: 'test',
+          LOCATION: 1,
+        })
+        .end((err, res) => {
+          res.should.have.status(400);
+        });
+    });
+    it('should return 400 when missing authentication', () => {
       chai.request('http://localhost:3000')
         .post('/updateSensorLocation')
         .send({
