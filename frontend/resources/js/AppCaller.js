25ada5832c8572b4219c39eb3900017eee400f99
Julius Figge <julius@figge.eu>
2020-10-23 10:19:21 +0200

    delete leftovers

--- frontend/resources/js/AppCaller.js
+++ frontend/resources/js/AppCaller.js
@@ -7,7 +7,3 @@ const app = new App.GeileTypenWetterApp(c.TEMPERATURE_COLOR, c.AIRPRESSURE_COLOR
   'airPressureNow', 'sensorForChartDropdown', '#yAxisToggleButton',
   'sensorPlottingLocation', 'temperatureNow', 'humidityNow', 'airPressureNow', 'sensorForChartDropdown');
 app.init();
-// create sensorTable
-const SensorTableFiller = require('./SensorTableFiller');
-const sensorTableFiller = new SensorTableFiller.SensorTableFiller(c.SERVER_URI);
-sensorTableFiller.init();
