b29350e6abf0123cc32ad8e8d8835edbe45f1fea
Julius Figge <julius@figge.eu>
2020-10-23 10:42:33 +0200

    remove script in order to avoid issues with permission

--- frontend/browserifyModules.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-#TODO what about the scripts for windows - may someone using it create it?
-cd resources/js;
-browserify AppCaller.js -o frontend.js;
-browserify AdminAppCaller.js -o frontend-admin.js;
