d4089a9fc28df80d8e8675f0bd78606afeb54be5
Julius Figge <julius@figge.eu>
2020-10-23 10:21:47 +0200

    add todo regarding windows

--- backend/buildImageandTar.sh
+++ backend/buildImageandTar.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+#TODO what about the scripts for windows - may someone using it create it?
 rm ../awe2backend.tar
 npm install
 rm -rf db/
--- frontend/buildImageandTar.sh
+++ frontend/buildImageandTar.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
+#TODO what about the scripts for windows - may someone using it create it?
 rm ../awe2frontend.tar
 npm install
 docker build -t awe2/frontend:beta .
