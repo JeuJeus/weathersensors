15ccb32ff18a6b00d81d6953987f29467cf1a559
Julius Figge <julius@figge.eu>
2020-10-25 19:28:14 +0100

    introduce filter by timerange function

--- backend/src/helper.js
+++ backend/src/helper.js
@@ -4,10 +4,17 @@ function reduceElementsToMaxSize(elements, maxSize) {
   if (elements.length <= maxSize) return elements;
   const origSize = elements.length;
   return Array.from({length: maxSize}, (_, i) =>
-    elements[Math.floor(i * (origSize + Math.floor(origSize/maxSize))/maxSize)]);
+    elements[Math.floor(i * (origSize + Math.floor(origSize / maxSize)) / maxSize)]);
 }
 
+function filterDateRange(elements, startTimeStamp, endTimestamp) {
+  return elements.filter(function(dataEntry) {
+    return dataEntry.TIMESTAMP >= startTimeStamp &&
+      dataEntry.TIMESTAMP <= endTimestamp;
+  });
+}
 
 module.exports = {
   'reduceElementsToMaxSize': reduceElementsToMaxSize,
+  'filterDateRange': filterDateRange,
 };
