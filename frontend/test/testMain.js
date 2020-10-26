62843aae03f860d3191c537148e10e7d4c3d1c1a
pbrawe <philipproering94@gmail.com>
2020-10-26 16:54:42 +0100

    fixed tests to match new constructor

--- frontend/test/testMain.js
+++ frontend/test/testMain.js
@@ -34,13 +34,10 @@ describe('-- APP TESTS -- ', () => {
 
   describe('creating our App', () => {
     it('creates the App in node which has correct SERVER_URI', () => {
-      const app = new App.GeileTypenWetterApp(c.SERVER_URI,
-          '#granularity', '#yAxisToggleButton', '#sensorPlotting',
-          '#sensorPlottingLocation', '#temperatureNow', '#humidityNow',
-          '#airPressureNow', '#sensorForChartDropdown', '#dateTimeRange');
-      app.init();
+      const app = new App.GeileTypenWetterApp(c.SERVER_URI, '#dateTimeRange');
       app.setColors(c.TEMPERATURE_COLOR, c.AIRPRESSURE_COLOR, c.HUMIDITY_COLOR);
       app.setUpdateInterval(c.UPDATE_INTERVAL);
+      app.init();
       expect(app.serverURI).to.deep.equal(App.constants.SERVER_URI);
     });
   });
