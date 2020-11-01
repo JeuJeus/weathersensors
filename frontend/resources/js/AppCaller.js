83d5b7eb46642bfcf6745e41a0c893d60686933e
JonathanBrockhausen <mail@jbrockhausen.de>
2020-11-01 17:17:22 +0100

    refactored frontend to properly name html elements

--- frontend/resources/js/AppCaller.js
+++ frontend/resources/js/AppCaller.js
@@ -3,10 +3,10 @@ const App = require('./GeileTypenWetterApp');
 // creating app
 const app = new App.GeileTypenWetterApp(c.SERVER_URI);
 
-app.setDomElements('#granularity', '#yAxisToggleButton', '#sensorPlotting',
-    '#sensorPlottingLocation', '#temperatureNow', '#humidityNow',
-    '#airPressureNow', '#sensorForChartDropdown', '#resetRangeButton',
-    '#chart-container', '#dateTimeRange');
+app.setDomElements('#granularity-input', '#y-axis-toggle-button', '#sensor-id-text',
+    '#sensor-location-text', '#temperature-current-value', '#humidity-current-value',
+    '#airpressure-current-value', '#sensor-dropdown', '#reset-range-button',
+    '#chart-container', '#date-time-range-picker');
 app.setColors(c.TEMPERATURE_COLOR, c.AIRPRESSURE_COLOR, c.HUMIDITY_COLOR);
 app.setUpdateInterval(c.UPDATE_INTERVAL);
 app.init();
