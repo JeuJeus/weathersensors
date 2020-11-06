1f49f3d7f1c6c7a845a73ed5eb3950e58ca83aaf
Julius Figge <julius@figge.eu>
2020-11-06 14:06:54 +0100

    adjust length of warning display time

--- frontend/resources/js/Alert.js
+++ frontend/resources/js/Alert.js
@@ -1,7 +1,7 @@
 function showAndDismissAlert(type, message) {
   const htmlAlert = '<div class="alert alert-' + type + '">' + message + '</div>';
   $('.alert-messages').prepend(htmlAlert);
-  $('.alert-messages .alert').first().hide().fadeIn(200).delay(2000).fadeOut(1000, function() {
+  $('.alert-messages .alert').first().hide().fadeIn(200).delay(5000).fadeOut(1000, function() {
     $(this).remove();
   });
 }
