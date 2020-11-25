45df64764c660fa2931f0c8bc0c3c7adb83f66e6
Julius Figge <julius@figge.eu>
2020-11-25 17:34:07 +0100

    introduce visual representation based on trend for every sensor

--- frontend/resources/js/AppTrend.js
+++ frontend/resources/js/AppTrend.js
@@ -1,9 +1,25 @@
-function calculateTrends(sensorData, specificSensor, granularity) {
+function updateTrends(sensorData, granularity, temperatureTrend, humidityTrend, airpressureTrend) {
+  //TODO check whether these 'relativeFactors' need to be adapted over time
+  setTrend(temperatureTrend, 10000, calculateTrend(sensorData, sensorData.temperature, granularity));
+  setTrend(humidityTrend, 10000, calculateTrend(sensorData, sensorData.humidity, granularity));
+  setTrend(airpressureTrend, 10000, calculateTrend(sensorData, sensorData.airPressure, granularity));
+}
+
+function setTrend(specificSensor, relativeFactor, trendValue) {
+  let toCompare = (relativeFactor * parseFloat(trendValue));
+  if (toCompare <= (-0.1)) {
+    specificSensor.classList = 'trendFalling';
+  } else if ((-0.1) <= toCompare && toCompare <= 0.1) {
+    specificSensor.classList = 'trendStagnant';
+  } else if (0.1 <= toCompare) {
+    specificSensor.classList = 'trendRising';
+  }
+}
+
+function calculateTrend(sensorData, specificSensor, granularity) {
   let dataLength = getLowerBoundGranularityOrSensordataLength(sensorData, granularity);
   let trendData = createDataPointTuples(sensorData.timestamps.slice(-dataLength), specificSensor.slice(-dataLength), dataLength);
-  console.log(trendData);
-  let trendTemperature = ss.linearRegression(trendData);
-  console.log(trendTemperature);
+  return ss.linearRegression(trendData).m;
 }
 
 function createDataPointTuples(timestamps, specificSensor, length) {
@@ -17,5 +33,5 @@ function getLowerBoundGranularityOrSensordataLength(sensorData, granularity) {
 }
 
 module.exports = {
-  calculateTrends: calculateTrends,
+  updateTrends: updateTrends,
 };
