4d95afac31614f886a79ce05f86a9fcc5deb12c7
JonathanBrockhausen <mail@jbrockhausen.de>
2020-12-01 10:39:17 +0100

    added tooltip to data trends for better clarity

--- frontend/resources/js/AppTrend.js
+++ frontend/resources/js/AppTrend.js
@@ -2,26 +2,29 @@ const moment = require('moment-timezone');
 const statistics = require('simple-statistics');
 
 function updateTrends(sensorData, granularity, temperatureTrend, humidityTrend, airpressureTrend) {
-  //TODO check whether these 'relativeFactors' need to be adapted over time
-  setTrend(temperatureTrend, 10000, calculateTrend(sensorData, sensorData.temperature, granularity));
-  setTrend(humidityTrend, 10000, calculateTrend(sensorData, sensorData.humidity, granularity));
-  setTrend(airpressureTrend, 10000, calculateTrend(sensorData, sensorData.airPressure, granularity));
+  // TODO check whether these 'relativeFactors' need to be adapted over time
+  setTrend(temperatureTrend, 10000, calculateTrend(sensorData, sensorData.temperature, granularity), 'Temperature');
+  setTrend(humidityTrend, 10000, calculateTrend(sensorData, sensorData.humidity, granularity), 'Humidity');
+  setTrend(airpressureTrend, 10000, calculateTrend(sensorData, sensorData.airPressure, granularity), 'Air Pressure');
 }
 
-function setTrend(specificSensor, relativeFactor, trendValue) {
-  let toCompare = (relativeFactor * parseFloat(trendValue));
+function setTrend(specificSensor, relativeFactor, trendValue, dataType) {
+  const toCompare = (relativeFactor * parseFloat(trendValue));
   if (toCompare <= (-0.1)) {
     specificSensor.classList = 'trendFalling';
+    specificSensor.title = dataType + ' is trending downwards.';
   } else if ((-0.1) <= toCompare && toCompare <= 0.1) {
     specificSensor.classList = 'trendStagnant';
+    specificSensor.title = dataType + ' is stagnant.';
   } else if (0.1 <= toCompare) {
     specificSensor.classList = 'trendRising';
+    specificSensor.title = dataType + ' is trending upwards.';
   }
 }
 
 function calculateTrend(sensorData, specificSensor, granularity) {
-  let dataLength = getLowerBoundGranularityOrSensordataLength(sensorData, granularity);
-  let trendData = createDataPointTuples(sensorData.timestamps.slice(-dataLength), specificSensor.slice(-dataLength), dataLength);
+  const dataLength = getLowerBoundGranularityOrSensordataLength(sensorData, granularity);
+  const trendData = createDataPointTuples(sensorData.timestamps.slice(-dataLength), specificSensor.slice(-dataLength), dataLength);
   return statistics.linearRegression(trendData).m;
 }
 
