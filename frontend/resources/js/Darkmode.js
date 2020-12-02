c8e41e40011038ea6cfac17e6af8e0481dd61ef4
Julius Figge <julius@figge.eu>
2020-12-02 20:55:52 +0100

    added darkmode rules for current date and options

--- frontend/resources/js/Darkmode.js
+++ frontend/resources/js/Darkmode.js
@@ -1,6 +1,7 @@
 function onDarkmodeButtonPress() {
   let pagestyle = document.getElementById('pagestyle');
   let currentStylesheet = pagestyle.getAttribute('href');
+
   let newStylesheet = (currentStylesheet === '../resources/css/stylez.css') ? '../resources/css/stylez-darkmode.css' : '../resources/css/stylez.css';
   pagestyle.setAttribute('href', newStylesheet);
 }
