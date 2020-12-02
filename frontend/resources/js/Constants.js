73e9bca4f1e1e9f70c81503098fc128f056858a5
pbrawe <philipproering94@gmail.com>
2020-12-02 21:23:49 +0100

    fixed typo and adjusted value

--- frontend/resources/js/Constants.js
+++ frontend/resources/js/Constants.js
@@ -3,7 +3,7 @@ const UPDATE_INTERVAL = 1000 * 60;// one minute
 const LATEST_N_DATAPOINTS_AMOUNT = 10;
 
 const TEMPERATURE_COLOR = 'rgba(204,0,112,0.7)';
-const HUMIDITY_COLOR = 'rgba(100, 100, 255, 0,5)';
+const HUMIDITY_COLOR = 'rgba(100, 100, 255, 0.7)';
 const AIRPRESSURE_COLOR = 'rgba(0,204,109,0.8)';
 
 const TRAFFIC_LIGHT_CLASSES = {
