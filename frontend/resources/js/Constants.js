e3650fe61e7886bc042406d95b64f13be122e98f
pbrawe <philipproering94@gmail.com>
2020-12-02 21:12:30 +0100

    fixed small p***s by clicking on an a shady ad and buying a certain product. At first it hurt a bit but then I got used to it. It actually turned out very well. Increased size by > 50% and made my wife happy for the day.

--- frontend/resources/js/Constants.js
+++ frontend/resources/js/Constants.js
@@ -2,9 +2,9 @@ const SERVER_URI = 'https://awe2-api.jeujeus.de';
 const UPDATE_INTERVAL = 1000 * 60;// one minute
 const LATEST_N_DATAPOINTS_AMOUNT = 10;
 
-const TEMPERATURE_COLOR = 'rgb(204,0,112,0.7)';
-const HUMIDITY_COLOR = 'rgb(0,39,191,0.6)';
-const AIRPRESSURE_COLOR = 'rgb(0,204,109,0.8)';
+const TEMPERATURE_COLOR = 'rgba(204,0,112,0.7)';
+const HUMIDITY_COLOR = 'rgba(100, 100, 255, 0,5)';
+const AIRPRESSURE_COLOR = 'rgba(0,204,109,0.8)';
 
 const TRAFFIC_LIGHT_CLASSES = {
   'RED' : 'redTrafficLight',
