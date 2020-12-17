1e5590487dacbc33b58aa22debb239fe973616e6
Julius Figge <julius@figge.eu>
2020-12-17 11:36:43 +0100

    introduce job validation

--- backend/src/cron.js
+++ backend/src/cron.js
@@ -3,7 +3,15 @@ const alert = require('./inactivityEmailAlert');
 const env = require('./env');
 const log = require('./logger');
 
-cron.schedule(env.INACTIVITY_CRON_SCHEDULE, () => {
-  log.logWrite('INFO', 'STARTING INACTIVITY CHECK CRON JOB!');
-  alert.checkAndAlertInactiveSensors();
-});
+let validationResult = cron.validate(env.INACTIVITY_CRON_SCHEDULE);
+let success = validationResult ? 'INFO' : 'ERROR';
+let validity = validationResult ? 'VALID' : 'INVALID';
+let state = validationResult ? 'ENABLED' : 'DISABLED';
+log.logWrite(success, `CRONJOB FOR INACTIVITY VALIDATION WAS [${validity}], JOB IS [${state}]`);
+
+if (validationResult) {
+  cron.schedule(env.INACTIVITY_CRON_SCHEDULE, () => {
+    log.logWrite('INFO', 'STARTING INACTIVITY CHECK CRON JOB!');
+    alert.checkAndAlertInactiveSensors();
+  });
+}
