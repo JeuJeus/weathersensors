006f20170e49e9bf30269a4ef5e52c8e1987e3e4
Julius Figge <julius@figge.eu>
2020-12-27 09:47:19 +0100

    add production flag to build

--- backend/buildImageandTar.sh
+++ backend/buildImageandTar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm ../weathersensors-backend.tar
-npm install
+npm install --production
 npm run browserify
 docker build -t weathersensors/backend:latest . -f dockerfile
 docker save weathersensors/backend:latest > ../weathersensors-backend.tar
--- frontend/buildImageandTar.sh
+++ frontend/buildImageandTar.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 rm ../weathersensors-frontend.tar
-npm install
+npm install --production
 npm run browserify
 docker build -t weathersensors/frontend:latest . -f dockerfile
 docker save weathersensors/frontend:latest > ../weathersensors-frontend.tar
